4f81cc3a2b883e116f1e99afca359549ee7ab5c3
Flandre <flantan233@gmail.com>
2021-12-02 17:39:30 +0800

    update week2 day2

--- week2/day2/complex-ds/BinarySearchTree.ts
+++ week2/day2/complex-ds/BinarySearchTree.ts
@@ -84,24 +84,30 @@ class BinarySearchTree implements TreeInterface {
     }
   }
   rotateLeft(node: Node) {
-    if(this.root == node){
-      this.root = node.right
-    }
-    let tmp = node
-    node = node.right!
+    let tmp = new Node('')
+    this.copyNode(tmp, node)
+    this.copyNode(node, node.right!)
     tmp.right = node.left
     tmp.height = this.getNodeDepth(tmp)
     node.left = tmp
   }
   rotateRight(node: Node) {
-    if(this.root == node){
-      this.root = node.left
-    }
-    let tmp = node
-    node = node.left!
+    let tmp = new Node('')
+    this.copyNode(tmp, node)
+    this.copyNode(node, node.left!)
     tmp.left = node.right
     tmp.height = this.getNodeDepth(tmp)
     node.right = tmp
+  }
+  copyNode(target: Node | null, source: Node | null) {
+    if(source && target) {
+      target.value = source.value
+      target.left = source.left
+      target.right = source.right
+      target.height = source.height
+    } else {
+      target = source
+    }
   }
   getNodeDepth(node: Node|null, depth = 0): number {
     if(node) {
@@ -156,9 +162,9 @@ let bst = new BinarySearchTree()
 // bst.add('90')
 // bst.add('98')
 
-bst.add('50')
-bst.add('25')
-bst.add('35')
+// bst.add('50')
+// bst.add('25')
+// bst.add('35')
 /*
    50
   /
@@ -183,14 +189,14 @@ bst.add('35')
 // bst.add('5')
 
 
-// bst.add('50')
-// bst.add('60')
-// bst.add('70')
-// bst.add('40')
-// bst.add('55')
-// bst.add('30')
-// bst.add('57')
-// bst.add('56')
+bst.add('50')
+bst.add('60')
+bst.add('70')
+bst.add('40')
+bst.add('55')
+bst.add('30')
+bst.add('57')
+bst.add('56')
 
 
 // bst.add('55')
@@ -206,4 +212,4 @@ bst.add('35')
 console.log(bst.root)
 // bst.add('1')
 // console.log(bst.root)
-// console.log(bst.inOrderTraversal())
+console.log(bst.inOrderTraversal())
